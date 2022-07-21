540994b9b5113ec581174f1878f9e5a35c355515
casio9 <w0107748@umail.usq.edu.au>
2022-07-22 00:33:46 +0930

    Sixth commit

---
+++ Week1_Intro_Coding.py
@@ -0,0 +1,5 @@
+# 1. Write in python that prints your name, and then counts up to your favourite number squared.
+print("Christopher Webb")
+# favourite number is 23
+for i in range(1,24):
+    print(i)
