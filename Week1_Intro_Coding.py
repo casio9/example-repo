294e8963590405b7ae72c9de2811921a36a13939
casio9 <w0107748@umail.usq.edu.au>
2022-07-22 20:25:34 +0930

    MEC4406 Intro Code commit 1 of 1

--- Week1_Intro_Coding.py
+++ Week1_Intro_Coding.py
@@ -1,5 +1,40 @@
 # 1. Write in python that prints your name, and then counts up to your favourite number squared.
-print("Christopher Webb")
-# favourite number is 23
-for i in range(1,24):
-    print(i)
+print("Print my name!")
+myname = "Chris"
+print(myname)
+# favourite number is 64
+print("My favourite number is 64. Let's count in squares!..")
+for i in range(1,9):
+    print(i*i)
+## 3. Update the code to contain a class Engineers, with a fixed class attribute skill equal to "problem solver", and initial attributes name, type and years of experience. Create two different engineers (objects) and print out their attributes.
+print("Now let's update the code to contain a class called ""Engineers"" with attributes")
+
+
+print("\n")
+class Engineers:
+    def __init__(self, name, type, years):
+        self.name = name
+        self.type = type
+        self.years = years
+    def getName(self):
+        print("Name is "+ self.name)
+    def getType(self):
+        print("Type is "+ self.type)
+    def getYears(self):
+        print(self.years + " years of experience \n\n")
+
+e1 = Engineers("John Smith", "mechnical", "14")
+e1.getName()
+e1.getType()
+e1.getYears()
+
+e1 = Engineers("Kate Jones", "electrical", "5")
+e1.getName()
+e1.getType()
+e1.getYears()
+print("\n")
+print('Now we will reverse my name Chris to sirhC...')
+def NameBackwards(x):
+    return x[::-1]
+myNameBackwards = NameBackwards(myname)
+print(myNameBackwards)
